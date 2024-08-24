83952bcd78eab8ca891db559cec493cda97e7f33
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-08-24 10:07:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function convertToRoman(num) {
   	const romanNumerals = [
       ['M',1000], 
-	  ['CM', 900]
+	  ['CM', 900],
       ['D', 500],
 	  ['CD', 400]
       ['C', 100], 
-	  ['XC', 90]
+	  ['XC', 90],
       ['L', 50],
 	  ['XL', 40],
       ['X', 10], 
