e66a80fa8e5f4fd66dbd752856d4b967f445536f
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-09-07 15:15:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-function convertToRoman(num) {
-  	const romanNumerals = [
-      ['M',1000], 
-	  ['CM', 900],
-      ['D', 500],
-	  ['CD', 400],
-      ['C', 100], 
-	  ['XC', 90],
-      ['L', 50],
-	  ['XL', 40],
-      ['X', 10], 
-	  ['IX', 9],
-      ['V', 5], 
-	  ['IV', 4],
-      ['I', 1]
-    ];
-
-  //your code here
-	let result = '';
-
-	for(let i = 0; i < romanNumerals.length; i++)
+function converToRoman(num) {
+	const obj = 
 		{
-			const [symbol, value] = romanNumerals[i];
-        while (num >= value) {
-            result += symbol;
-            num -= value;
+	'M':1000,
+	'CM': 900,
+    'D': 500,
+    'CD': 400,
+    'C': 100,
+    'XC': 90,
+    'L': 50,
+    'XL': 40,
+    'X': 10,
+    'IX': 9,
+    'V': 5,
+    'IV': 4,
+    'I': 1
+		};
+
+	let result = "";
+	for(let i in obj)
+		{
+			while (num >= obj[i]) {
+				result += i;
+				num -= obj[i];
 			}
 		}
 	return result;
 }
+
+console.log(converToRoman(return));
+module.exports = converToRoman;
+
